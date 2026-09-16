00b0fcbb5937681e2f2c2200a3254516ce789c8c
Lavanya ArorA <lavanyaarora06242gmail.com>
2026-09-16 23:01:56 +0530

    Local changes to be committed:

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (() => {
-  const API_BASE = "https://mental-health-score-zlu7.onrender.com";
+  const API_BASE = "http://127.0.0.1:2200";
 
   const form = document.getElementById("predictForm");
   const predictBtn = document.getElementById("predictBtn");
